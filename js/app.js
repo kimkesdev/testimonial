b6e3e33b5d2a034efd998a10ec8236f6573ab6f7
kimkesdev <Kimboelens@gmail.com>
2021-02-17 10:30:23 +0000

    customers

--- js/app.js
+++ js/app.js
@@ -26,11 +26,11 @@
     }
 
     
-    createCustomer(0, 'John', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis neque reprehenderit laborum, corporis explicabo assumenda. Porro impedit consectetur animi, reprehenderit recusandae sapiente at aliquam reiciendis modi ipsam rerum suscipit distinctio?')
-    createCustomer(1, 'Sandy', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock')
-    createCustomer(2, 'Amy', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.')
-    createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
-    createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
+    createCustomer(0, 'Kim', 'It is easy to use, happy I found it.')
+    createCustomer(1, 'Sandy', 'If I can use it, everybody can use it!')
+    createCustomer(2, 'Amy', 'Testimonial by Amy')
+    createCustomer(3, 'Joseph', 'Josephs testimonial')
+    createCustomer(4, 'Diana', 'My feedback')
     
 
     buttons.forEach(function(button){
